efbe58dd1570f883d397fe20cf58d799d76295ed
Christian Trutz <info@trutz-software-consulting.de>
2021-02-11 17:24:21 +0000

    Lektion 1 eingeführt.

---
+++ lektion1/src/main/java/lektion1/DemoApplication.java
@@ -0,0 +1,24 @@
+package lektion1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements ApplicationRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        LOG.info("MongoDB lernen - Lektion 1 gestartet mit folgenden Parameter : {}", args.getOptionNames());
+    }
+
+}
