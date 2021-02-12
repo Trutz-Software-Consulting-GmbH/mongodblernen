2d661f79a6f9a0e262e83fd0b9a3ce8787f8ce14
Christian Trutz <info@trutz-software-consulting.de>
2021-02-12 11:45:05 +0000

    DemoApplication erster Wurf

--- lektion1/src/main/java/lektion1/DemoApplication.java
+++ lektion1/src/main/java/lektion1/DemoApplication.java
@@ -1,11 +1,17 @@
 package lektion1;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class DemoApplication implements ApplicationRunner {
@@ -16,9 +22,25 @@ public class DemoApplication implements ApplicationRunner {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Autowired
+    MongoClient mongoClient;
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         LOG.info("MongoDB lernen - Lektion 1 gestartet mit folgenden Parameter : {}", args.getOptionNames());
+
+        // Alle Datenbanken und aktuell verwendete Datenbank loggen
+        mongoClient.listDatabaseNames().forEach(dbName -> LOG.info("Database {}", dbName));
+        LOG.info("Current Database {}", mongoTemplate.getDb().getName());
+
+        // Ein Document einfügen
+        MongoCollection<Document> personenCollection = mongoTemplate.getDb().getCollection("personen");
+        Document johndoe = new Document();
+        johndoe.append("name", "John Doe");
+        personenCollection.insertOne(johndoe);
     }
 
 }
