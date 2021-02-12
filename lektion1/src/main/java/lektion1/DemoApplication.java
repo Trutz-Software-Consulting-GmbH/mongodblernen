3f83fc42425219d4049301d0326aa95cd017d5a0
Christian Trutz <info@trutz-software-consulting.de>
2021-02-12 13:53:35 +0000

    Spring Boot Tests anstelle von Start der Anwendung

--- lektion1/src/main/java/lektion1/DemoApplication.java
+++ lektion1/src/main/java/lektion1/DemoApplication.java
@@ -1,50 +1,13 @@
 package lektion1;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoCollection;
-import static com.mongodb.client.model.Filters.*;
-
-import org.bson.Document;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.core.MongoOperations;
 
 @SpringBootApplication
-public class DemoApplication implements ApplicationRunner {
-
-    private static final Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
+public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    @Autowired
-    MongoClient mongoClient;
-
-    @Autowired
-    MongoOperations mongoOps;
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        LOG.info("MongoDB lernen - Lektion 1 gestartet mit folgenden Parameter : {}", args.getOptionNames());
-
-        // Alle Datenbanken loggen
-        mongoClient.listDatabaseNames().forEach(dbName -> LOG.info("Database {}", dbName));
-
-        // Ein Document einfügen
-        MongoCollection<Document> personen = mongoOps.getCollection("personen");
-        Document johndoe = new Document();
-        johndoe.append("name", "John Doe");
-        personen.insertOne(johndoe);
-
-        // Zuvor eingefügtes Document finden 
-        personen.find(eq("name", "John Doe")).limit(1).iterator()
-                .forEachRemaining(document -> LOG.info("name={}", document.get("name")));
     }
 
 }
--- lektion1/src/test/java/lektion1/DemoApplicationTests.java
+++ lektion1/src/test/java/lektion1/DemoApplicationTests.java
@@ -1,13 +1,47 @@
 package lektion1;
 
+import static com.mongodb.client.model.Filters.eq;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoOperations;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(DemoApplicationTests.class);
+
+    @Autowired
+    MongoClient mongoClient;
+
+    @Autowired
+    private MongoOperations mongoOps;
+
+    @BeforeEach
+    void beforeEach() {
+        // Alle Datenbanken loggen
+        mongoClient.listDatabaseNames().forEach(dbName -> LOG.info("Database {}", dbName));
+    }
+
+    @Test
+    void insertFindTest() {
+        // Ein Document einfügen
+        MongoCollection<Document> personen = mongoOps.getCollection("personen");
+        Document johndoe = new Document();
+        johndoe.append("name", "John Doe");
+        personen.insertOne(johndoe);
+
+        // Zuvor eingefügtes Document finden
+        personen.find(eq("name", "John Doe")).limit(1).iterator()
+                .forEachRemaining(document -> LOG.info("name={}", document.get("name")));
+    }
 
 }
