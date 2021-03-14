b7d5f80272110717256eedf1fda51f63f144cf16
Christian Trutz <info@trutz-software-consulting.de>
2021-03-14 09:54:03 +0000

    Test insertViaNativeAPI() hinzugefügt (Achtung Test schlägt absichtlich fehl)

--- src/test/java/lektion/InsertTests.java
+++ src/test/java/lektion/InsertTests.java
@@ -3,7 +3,10 @@ package lektion;
 import java.util.stream.Stream;
 
 import com.mongodb.WriteConcern;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
 
+import org.bson.Document;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +18,20 @@ import org.springframework.util.StopWatch;
 public class InsertTests {
 
     @Autowired
+    private MongoClient mongoClient;
+
+    @Autowired
     private MongoOperations mongoOps;
+
+    @Test
+    void insertViaNativeAPI() {
+        Document me = new Document();
+        me.append("name", "Christian Trutz");
+        MongoCollection<Document> personen = mongoClient.getDatabase("test").getCollection("personen");
+        Stream.iterate(1, n -> n + 1).limit(10_000).forEach(i -> {
+            personen.insertOne(me);
+        });
+    }
 
     @Test
     void insertViaMongoOps() {
