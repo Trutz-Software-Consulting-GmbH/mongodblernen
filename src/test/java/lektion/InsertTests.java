5bc9be6d5f53b47497009bb98cb236d17fb5e1ca
Christian Trutz <info@trutz-software-consulting.de>
2021-03-14 11:15:04 +0000

    StopWatch in @BeforeAll und @AfterAll Methoden ausgelagert, so dass jetzt der gesammte Test aufgeführt werden kann und die Performance der einzelnen WriteConcerns verglichen werden kann

--- src/test/java/lektion/InsertTests.java
+++ src/test/java/lektion/InsertTests.java
@@ -2,10 +2,13 @@ package lektion;
 
 import java.util.stream.Stream;
 
+import com.mongodb.WriteConcern;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 
 import org.bson.Document;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,19 +24,57 @@ public class InsertTests {
     @Autowired
     private MongoClient mongoClient;
 
+    private static StopWatch stopWatch;
+
+    @BeforeAll
+    static void beforeAll() {
+        stopWatch = new StopWatch("InsertTests stop watch");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        LOGGER.debug(stopWatch.prettyPrint());
+    }
+
     @Test
     void insertViaNativeAPI() {
         MongoCollection<Document> personen = mongoClient.getDatabase("test").getCollection("personen");
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start("10.000 Dokumente mit Hilfe der nativen MongoDB Java API");
+        stopWatch.start(
+                "10.000 Dokumente hinzugefügt, WriteConcern undefiniert = w:1, j:false");
         Stream.iterate(1, n -> n + 1).limit(10_000).forEach(i -> {
             Document me = new Document();
             me.append("name", "Christian Trutz");
             personen.insertOne(me);
         });
         stopWatch.stop();
-        LOGGER.debug("10.000 Dokumente mit Hilfe der nativen MongoDB Java API hinzugefügt.");
-        LOGGER.debug(stopWatch.prettyPrint());
+    }
+
+    @Test
+    void insertViaNativeAPIAndWriteConcernUNACKNOWLEDGED() {
+        MongoCollection<Document> personen = mongoClient.getDatabase("test").getCollection("personen")
+                .withWriteConcern(WriteConcern.UNACKNOWLEDGED);
+        stopWatch.start(
+                "10.000 Dokumente hinzugefügt, WriteConcern = UNACKNOWLEDGED = w:0,j:false");
+        Stream.iterate(1, n -> n + 1).limit(10_000).forEach(i -> {
+            Document me = new Document();
+            me.append("name", "Christian Trutz");
+            personen.insertOne(me);
+        });
+        stopWatch.stop();
+    }
+
+    @Test
+    void insertViaNativeAPIAndWriteConcernJOURNALED() {
+        MongoCollection<Document> personen = mongoClient.getDatabase("test").getCollection("personen")
+                .withWriteConcern(WriteConcern.JOURNALED);
+        stopWatch.start(
+                "10.000 Dokumente hinzugefügt, WriteConcern = JOURNALED = w:1,j:true");
+        Stream.iterate(1, n -> n + 1).limit(10_000).forEach(i -> {
+            Document me = new Document();
+            me.append("name", "Christian Trutz");
+            personen.insertOne(me);
+        });
+        stopWatch.stop();
     }
 
 }
