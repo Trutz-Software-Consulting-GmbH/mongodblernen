0575a5813b70f135c0e677f58dd5d4349e322a99
Christian Trutz <info@trutz-software-consulting.de>
2021-03-14 09:54:03 +0000

    Test insertViaNativeAPI() hinzugefügt (Achtung Test schlägt absichtlich fehl)

--- src/test/java/lektion/InsertTests.java
+++ src/test/java/lektion/InsertTests.java
@@ -2,37 +2,28 @@ package lektion;
 
 import java.util.stream.Stream;
 
-import com.mongodb.WriteConcern;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
 
+import org.bson.Document;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.util.StopWatch;
 
 @SpringBootTest
 public class InsertTests {
 
     @Autowired
-    private MongoOperations mongoOps;
+    private MongoClient mongoClient;
 
     @Test
-    void insertViaMongoOps() {
-
-        ((MongoTemplate) mongoOps).setWriteConcern(WriteConcern.UNACKNOWLEDGED);
-        mongoOps.insert(new Person());
-
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start("Insert via MongoTemplate");
+    void insertViaNativeAPI() {
+        Document me = new Document();
+        me.append("name", "Christian Trutz");
+        MongoCollection<Document> personen = mongoClient.getDatabase("test").getCollection("personen");
         Stream.iterate(1, n -> n + 1).limit(10_000).forEach(i -> {
-            Person me = new Person();
-            me.name = "Christian Trutz";
-            mongoOps.insert(me);
+            personen.insertOne(me);
         });
-        stopWatch.stop();
-
-        System.out.println(stopWatch.getTotalTimeSeconds());
     }
 
 }
