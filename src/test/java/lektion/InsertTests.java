62d6faac7feb892480b0239a240f7392db7ebfd3
Christian Trutz <info@trutz-software-consulting.de>
2021-03-14 10:51:55 +0000

    StopWatch für die Zeitmessung eingeführt, Test insertViaNativeAPI() erneut mit Zeitmessung ausgeführt und Auswertung der Zeit mit stopWatch.prettyPrint()

--- src/test/java/lektion/InsertTests.java
+++ src/test/java/lektion/InsertTests.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StopWatch;
 
 @SpringBootTest
 public class InsertTests {
@@ -23,12 +24,16 @@ public class InsertTests {
     @Test
     void insertViaNativeAPI() {
         MongoCollection<Document> personen = mongoClient.getDatabase("test").getCollection("personen");
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("10.000 Dokumente mit Hilfe der nativen MongoDB Java API");
         Stream.iterate(1, n -> n + 1).limit(10_000).forEach(i -> {
             Document me = new Document();
             me.append("name", "Christian Trutz");
             personen.insertOne(me);
         });
+        stopWatch.stop();
         LOGGER.debug("10.000 Dokumente mit Hilfe der nativen MongoDB Java API hinzugefügt.");
+        LOGGER.debug(stopWatch.prettyPrint());
     }
 
 }
